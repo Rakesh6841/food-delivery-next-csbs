9712f3bae39a2c2d8104fc90fcc195c7ab541e3c
kunrak <kundur947@gmail.com>
2024-12-20 03:19:25 +0530

    Header & Footer

---
+++ app/restaurant/page.js
@@ -0,0 +1,31 @@
+"use client";
+
+import RestaurantLogin from "../_components/RestaurantLogin";
+import RestaurantSignup from "../_components/RestaurantSignUp";
+import RestaurantHeader from "../_components/RestaurantHeader";
+import Footer from "../_components/Footer";
+import { useState } from "react";
+import "./style.css"
+
+const Restaurant = () => {
+  const [login, setLogin] = useState(true);
+  return (
+    <>
+      <div className="container">
+        <RestaurantHeader />
+        <h1>Restaurant Login/Signup Page</h1>
+        {login ? <RestaurantLogin /> : <RestaurantSignup />}
+        <div>
+          <button className="button-link" onClick={() => setLogin(!login)}>
+            {login
+              ? "Do not have account? Signup"
+              : "Already have account? Login"}
+          </button>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Restaurant;
